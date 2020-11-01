af63e8062ba002411c1bac83f02f59219bcf521e
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-11-01 16:23:02 +0530

    Include provider at top level of component hierarchy

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { reducer, initialState } from './reducer';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import TaskList from './components/TaskList';
 import './App.css';
+import { AuthenticationProvider } from './AuthenticationContext';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-        <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-          <Route path="/tasks">
-            <TaskList />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <AuthenticationProvider reducer={reducer} initialState={initialState}>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            <Route path="/tasks">
+              <TaskList />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </AuthenticationProvider>
   );
 }
 
