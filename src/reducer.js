d22263cac736964a53b875cbd2d4f850d18ed3a4
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-11-01 16:18:57 +0530

    Add initial state and reducer function

---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+export const initialState = {
+    user: null
+}
+
+export const reducer = (state, action) => {
+    switch(action.type) {
+        case 'LOG_IN':
+            return state;
+
+        default:
+            return state;
+    }
+}
