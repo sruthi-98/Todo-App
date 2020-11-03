67067135ce07c5f1c8b2b5219cdcbcd2f9edad0c
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-11-03 19:24:49 +0530

    Add header in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../styles/Home.css';
+import HomeHeader from './HomeHeader';
 
 function Home() {
     return (
         <div className="home">
+            <HomeHeader />
             <h1>Organize with us</h1>
             <button>Get Started</button>
         </div>
