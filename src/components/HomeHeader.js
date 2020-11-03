4908579dd8faeb8c7131ba248dc6329eb9092e5d
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-11-03 19:24:22 +0530

    Add login and signup button

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function HomeHeader() {
+    const history = useHistory();
+
     return (
-        <div>
-            
+        <div className="homeHeader">
+            <span>ToDo Manager</span>
+            <button onClick={() => history.push('/login')}>Login</button>
+            <button onClick={() => history.push('/signup')}>Sign Up</button>
         </div>
     )
 }
