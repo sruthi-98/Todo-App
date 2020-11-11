7308342907811da49cca854d9016bf421c697dcd
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-11-11 19:09:25 +0530

    Add click handler for get started button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import HomeHeader from './HomeHeader';
 
 function Home() {
+    const history = useHistory();
+
     return (
         <div className="home">
             <HomeHeader />
             <h1 className="home__mainHeading">Organize with us</h1>
             <span className="home__subHeading">A task manager you can rely on</span>
-            <button className="home__button">Get Started</button>
+            <button className="home__button" onClick={() => history.push('/signup')}>Get Started</button>
             <img className="home__image" src={require('../images/todo.svg')} alt="Todo" />
         </div>
     )
