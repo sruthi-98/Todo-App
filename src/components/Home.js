b8804660e5ce916e2dccc28f23e3e4cb6d823043
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-11-11 15:02:53 +0530

    Add sub heading and class for image

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,9 @@ function Home() {
         <div className="home">
             <HomeHeader />
             <h1 className="home__mainHeading">Organize with us</h1>
+            <span className="home__subHeading">A task manager you can rely on</span>
             <button className="home__button">Get Started</button>
-            <img src={require('../images/todo.svg')} alt="Todo" />
+            <img className="home__image" src={require('../images/todo.svg')} alt="Todo" />
         </div>
     )
 }
