1e9c7f3e386f5ccbb69b0efc1f62ce5cc8193580
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-11-11 17:15:25 +0530

    Add additional class name for elements

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,15 +29,16 @@ function Login() {
 
     return (
         <div className="login">
-            <h1 className="login__title">ToDo Manager</h1>
+            <h1 className="login__header">ToDo Manager</h1>
         
             <div className="login__container">
-                <h1>Log In</h1>
+                <h1 className="login__title">Login</h1>
 
                 <form className="login__form">
                     <div className="login__formGroup">
-                        <label>E-mail</label>
+                        <label className="login__formLabel">E-mail</label>
                         <input 
+                            className="login__formInput"
                             type="text" 
                             value={email} 
                             onChange={e => setEmail(e.target.value)}
@@ -45,25 +46,28 @@ function Login() {
                     </div>
 
                     <div className="login__formGroup">
-                        <label>Password</label>
+                        <label className="login__formLabel">Password</label>
                         <input 
+                            className="login__formInput"
                             type="password" 
                             value={password} 
                             onChange={e => setPassword(e.target.value)}
                         />
                     </div>
                 
-                    <button
-                        type="submit"
-                        onClick={login}
-                        className="login__button"
-                    >
-                        Log In
-                    </button>
+                    <div className="login__formGroup">
+                        <button
+                            type="submit"
+                            onClick={login}
+                            className="login__button"
+                        >
+                            Log In
+                        </button>
 
-                    <p className="login__signup">
-                        Dont’t have an account? Sign up <Link to='/signup'>here</Link>
-                    </p>
+                        <p className="login__signup">
+                            Dont’t have an account? Sign up <Link className="login__link" to='/signup'>here</Link>
+                        </p>
+                    </div>
                 </form>
             </div>
         </div>
