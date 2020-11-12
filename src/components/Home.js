393e696a78397f19b54705f1f570823b55a455ae
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-11-12 21:09:18 +0530

    Create a new section for text and button

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,11 @@ function Home() {
     return (
         <div className="home">
             <HomeHeader />
-            <h1 className="home__mainHeading">Organize with us</h1>
-            <span className="home__subHeading">A task manager you can rely on</span>
-            <button className="home__button" onClick={() => history.push('/signup')}>Get Started</button>
+            <div className="home__section">
+                <h1 className="home__mainHeading">Organize with us</h1>
+                <span className="home__subHeading">A task manager you can rely on</span>
+                <button className="home__button" onClick={() => history.push('/signup')}>Get Started</button>
+            </div>
             <img className="home__image" src={require('../images/todo.svg')} alt="Todo" />
         </div>
     )
