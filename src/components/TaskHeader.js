2649f6269d85d2b9f0c9ce87b7a3b059bdd6eb3b
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-11-13 11:54:53 +0530

    Display user's name on task page

--- src/components/TaskHeader.js
+++ src/components/TaskHeader.js
@@ -4,9 +4,11 @@ import { useHistory } from 'react-router-dom';
 
 function TaskHeader() {
     const history = useHistory();
+    const name = localStorage.getItem('name');
 
     const logOut = () => {
         localStorage.removeItem('userId');
+        localStorage.removeItem('name');
         history.push('/');
     }
 
@@ -14,6 +16,7 @@ function TaskHeader() {
         <div className="taskHeader">
             <div className="taskHeader__section">
                 <span className="taskHeader__title">ToDo Manager</span>
+                <span className="taskHeader__name">Hello, {name}</span>
                 <ExitToAppIcon className="taskHeader__exit" onClick={logOut}></ExitToAppIcon>
             </div>
         </div>
