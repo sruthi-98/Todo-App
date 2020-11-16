4a5957e189a221bdeb6c0bc40a07a7b3a3d86910
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-11-16 11:39:27 +0530

    Add loading dots

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Loader() {
     return (
         <div className="loader">
-            
+            {[...Array(5)].map(index => (<span></span>))}
         </div>
     )
 }
